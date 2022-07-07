818dfb1b50ba7d2009cc7025965bdc71e2662c68
Lnola <nola.luka@gmail.com>
2022-07-07 10:24:02 +0200

    Messing with husky 🐶

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Realtime chat room app.
 ## To start the project
 
 ```
+pnpm install
 docker-compose up
 ```
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,8 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application } from 'express';
 
 const app: Application = express();
 
-const port: number = 3001;
+const port = 3001;
 
 app.listen(port, function () {
   console.log(`App is listening on port ${port} !`);
