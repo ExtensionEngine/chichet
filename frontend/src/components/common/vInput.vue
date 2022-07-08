77a1cd5333c8f5f5b5ccf3120288408aad3d2194
sandje00 <stella.andjelic.00@fesb.hr>
2022-07-08 10:55:47 +0200

    Add base input

---
+++ frontend/src/components/common/vInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <input @input="updateModelValue" :value="modelValue" class="input" />
+</template>
+
+<script>
+import useModelValue from '../../composables/useModelValue.js';
+
+export default {
+  name: 'v-input',
+  props: {
+    modelValue: { type: String, default: '' },
+  },
+  setup(_props, context) {
+    const { updateModelValue } = useModelValue(context);
+    return { updateModelValue };
+  },
+};
+</script>
+
+<style>
+.input {
+  font-size: inherit;
+  background: none;
+  border: none;
+}
+
+.input:focus {
+  outline: none;
+}
+</style>
---
+++ frontend/src/composables/useModelValue.js
@@ -0,0 +1,7 @@
+export default function useModelValue({ emit }) {
+  const updateModelValue = event => {
+    const val = event.target.value;
+    emit('update:modelValue', val);
+  };
+  return { updateModelValue };
+}
