ce885644e32f4464965e5b3274d99cf8e68f0aca
sandje00 <stella.andjelic.00@fesb.hr>
2022-07-08 11:08:40 +0200

    Add base field

---
+++ frontend/src/components/common/vField.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="field">
+    <label v-if="label">{{ label }}</label>
+    <slot><v-input v-bind="$attrs" /></slot>
+    <span class="error">{{ error }}</span>
+  </div>
+</template>
+
+<script>
+import vInput from './vInput.vue';
+
+export default {
+  name: 'v-field',
+  props: {
+    label: { type: String, default: '' },
+    error: { type: String, default: '' },
+  },
+  components: { vInput },
+};
+</script>
+
+<style>
+.error::after {
+  content: ' ';
+  font-size: 0;
+  white-space: pre;
+}
+</style>
