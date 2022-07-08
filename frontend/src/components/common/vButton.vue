8d59f9951c53585d5f04b9efb30fba6a08c06001
sandje00 <stella.andjelic.00@fesb.hr>
2022-07-08 10:34:41 +0200

    Add base button

---
+++ frontend/src/components/common/vButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="button">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'v-button',
+};
+</script>
+
+<style>
+.button {
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+</style>
