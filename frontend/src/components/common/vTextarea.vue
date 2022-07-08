05df4f2ad83e1c08c8e8023b192b0aee391265c3
sandje00 <stella.andjelic.00@fesb.hr>
2022-07-08 11:09:23 +0200

    Add base textarea

---
+++ frontend/src/components/common/vTextarea.vue
@@ -0,0 +1,27 @@
+<template>
+  <textarea @input="updateModelValue" :value="modelValue" class="textarea"></textarea>
+</template>
+
+<script>
+import useModelValue from '../../composables/useModelValue.js';
+
+export default {
+  name: 'base-input',
+  props: {
+    modelValue: { type: String, default: '' },
+  },
+  setup(_props, context) {
+    const { updateModelValue } = useModelValue(context);
+    return { updateModelValue };
+  },
+};
+</script>
+
+<style>
+.textarea {
+  font-size: inherit;
+  background: none;
+  outline: none;
+  resize: none;
+}
+</style>
