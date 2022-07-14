ea4b60eaaace280b8f2f14a5901fabafc1d13b00
Lnola <nola.luka@gmail.com>
2022-07-14 13:00:07 +0200

    Add default button primary and secondary styles

--- frontend/src/components/common/ChiButton.vue
+++ frontend/src/components/common/ChiButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="chi-button">
+  <button class="chi-button" :class="{ 'chi-button-secondary': secondary }">
     <slot></slot>
   </button>
 </template>
@@ -7,13 +7,27 @@
 <script>
 export default {
   name: 'chi-button',
+  props: {
+    secondary: { type: Boolean, default: false },
+  },
 };
 </script>
 
 <style scoped>
 .chi-button {
-  background: none;
+  background: var(--color-primary);
   border: none;
+  color: var(--color-secondary);
+  font: var(--font-accent);
+  text-transform: uppercase;
+  border-radius: 1.2rem;
+  padding: 1rem 1.5rem;
   cursor: pointer;
+}
+
+.chi-button-secondary {
+  background: transparent;
+  border: 2px solid var(--color-primary);
+  color: var(--color-primary);
 }
 </style>
