906c06594ca8bb0e0ff526252e8a7ba40b81132e
Lnola <nola.luka@gmail.com>
2022-07-19 10:00:07 +0200

    Display mock data

---
+++ frontend/src/components/tags/TagList.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <chi-button v-for="{ id, label, selected } in tags" :key="id" :secondary="!selected" class="button-round">
+      {{ label }}
+    </chi-button>
+  </section>
+</template>
+
+<script>
+import ChiButton from '../common/ChiButton.vue';
+export default {
+  name: 'tag-list',
+  props: {
+    tags: { type: Array, required: true },
+  },
+  components: { ChiButton },
+};
+</script>
+
+<style scoped>
+.button-round {
+  border-radius: 100px;
+}
+</style>
--- frontend/src/components/tags/TagSelect.vue
+++ frontend/src/components/tags/TagSelect.vue
@@ -2,12 +2,31 @@
   <main>
     <h1>Select your favorite music genres</h1>
     <h3>Tell us your favorite music genres so you see the rooms you want to visit first!</h3>
+
+    <tag-list :tags="tags"></tag-list>
   </main>
 </template>
 
 <script>
+import TagList from './TagList.vue';
 export default {
-  name: 'select-tags',
+  name: 'tag-select',
+  setup() {
+    const tags = [
+      { id: 1, label: 'Rock', selected: true },
+      { id: 2, label: 'Metal', selected: true },
+      { id: 3, label: 'Classical', selected: true },
+      { id: 4, label: 'Rock', selected: true },
+      { id: 5, label: 'Metal', selected: false },
+      { id: 6, label: 'Classical', selected: false },
+      { id: 7, label: 'Rock', selected: false },
+      { id: 8, label: 'Metal', selected: false },
+      { id: 9, label: 'Classical', selected: false },
+    ];
+
+    return { tags };
+  },
+  components: { TagList },
 };
 </script>
 
