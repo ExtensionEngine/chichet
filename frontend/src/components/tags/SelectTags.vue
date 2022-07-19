aeee7cbe65f4a4120161d2769b316c1c876164aa
Lnola <nola.luka@gmail.com>
2022-07-19 09:17:06 +0200

    Add tags route and main component

---
+++ frontend/src/components/tags/SelectTags.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'select-tags',
+};
+</script>
+
+<style scoped></style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import SelectTags from '@/components/tags/SelectTags.vue';
 import UserAuth from '@/components/auth/UserAuth.vue';
 
 const routes = [
@@ -6,6 +7,11 @@ const routes = [
     path: '/auth',
     name: 'UserAuth',
     component: UserAuth,
+  },
+  {
+    path: '/tags',
+    name: 'Tags',
+    component: SelectTags,
   },
 ];
 
