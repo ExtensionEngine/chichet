4af9873895e15733bacb1b740cc19a2577fcfe1b
Lnola <nola.luka@gmail.com>
2022-07-19 11:56:00 +0200

    Add basic axios request setup

---
+++ frontend/src/api/config.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const config = {
+  baseURL: import.meta.env.VITE_API_PATH,
+  headers: { 'Content-Type': 'application/json' },
+};
+
+const client = axios.create(config);
+
+export default client;
