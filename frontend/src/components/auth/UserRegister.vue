e1e44184009f29483cd1988875eb7bb9fa46b6d1
Lnola <nola.luka@gmail.com>
2022-07-25 14:32:00 +0200

    Implement register from form

--- frontend/src/components/auth/UserRegister.vue
+++ frontend/src/components/auth/UserRegister.vue
@@ -8,20 +8,36 @@
 </template>
 
 <script>
+import { auth as authApi } from '@/api';
 import { registerFormLabels } from './constants';
 import UserForm from './UserForm.vue';
+import { useRouter } from 'vue-router';
 
 export default {
   name: 'user-register',
   setup() {
+    const router = useRouter();
+
     const formInputs = [
-      { label: 'Username', type: 'text' },
-      { label: 'First name', type: 'text' },
-      { label: 'Last name', type: 'text' },
-      { label: 'Password', type: 'password' },
+      { label: 'Username', type: 'text', value: '' },
+      { label: 'First name', type: 'text', value: '' },
+      { label: 'Last name', type: 'text', value: '' },
+      { label: 'Password', type: 'password', value: '' },
     ];
 
-    const submit = () => {};
+    const submit = async () => {
+      const username = formInputs[0].value;
+      const firstName = formInputs[1].value;
+      const lastName = formInputs[2].value;
+      const password = formInputs[3].value;
+
+      try {
+        await authApi.register({ username, firstName, lastName, password });
+        router.push('/tags');
+      } catch (err) {
+        console.log(err.response.data.message);
+      }
+    };
 
     const { formButtonLabel, formSwitch } = registerFormLabels;
     return { formInputs, formButtonLabel, formSwitch, submit };
