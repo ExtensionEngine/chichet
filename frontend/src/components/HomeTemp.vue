25c6e1fd3699b6052e4c1deab683694c625f2afc
Lnola <nola.luka@gmail.com>
2022-07-26 16:16:14 +0200

    Add test sign out button to Home

--- frontend/src/components/HomeTemp.vue
+++ frontend/src/components/HomeTemp.vue
@@ -1,9 +1,20 @@
 <template>
-  <div></div>
+  <div>
+    <button @click="authStore.signOut(router)">Sign out</button>
+  </div>
 </template>
 
 <script>
+import { useAuthStore } from '../store/authStore';
+import { useRouter } from 'vue-router';
+
 export default {
   name: 'home-temp',
+  setup() {
+    const authStore = useAuthStore();
+    const router = useRouter();
+
+    return { authStore, router };
+  },
 };
 </script>
