63d1d74146584bfea7039f3544a7e88df7ea1c0b
Lnola <nola.luka@gmail.com>
2022-07-26 16:06:15 +0200

    Connect fe to endpoint

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -9,6 +9,9 @@ const urls = {
   get register() {
     return this.root + '/register';
   },
+  get signOut() {
+    return this.root + '/sign-out';
+  },
 };
 
 const signIn = credentials => {
@@ -19,7 +22,12 @@ const register = user => {
   return request.post(urls.register, user).then(extractData);
 };
 
+const signOut = () => {
+  return request.post(urls.signOut).then(extractData);
+};
+
 export default {
   signIn,
   register,
+  signOut,
 };
