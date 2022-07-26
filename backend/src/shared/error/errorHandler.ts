bda8564d0a9ae72129fe88cf664ba98563e8c891
sandje00 <stella.andjelic.00@fesb.hr>
2022-07-26 13:02:49 +0200

    Use only IHttpError as err argument type in errorHandler

--- backend/src/shared/error/errorHandler.ts
+++ backend/src/shared/error/errorHandler.ts
@@ -3,8 +3,8 @@ import { IAuthRequest } from 'shared/auth/types';
 import { IHttpError } from './types';
 import { INTERNAL_SERVER_ERROR } from 'http-status';
 
-const errorHandler = (err: IHttpError | Error, _req: IAuthRequest, res: Response, _next: NextFunction) => {
-  return 'status' in err
+const errorHandler = (err: IHttpError, _req: IAuthRequest, res: Response, _next: NextFunction) => {
+  return err.status
     ? res.status(err.status).json({ message: err.message })
     : res.status(INTERNAL_SERVER_ERROR).json({ message: 'Something went wrong' });
 };
