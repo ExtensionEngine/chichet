9eafe8d637e914a0c46df6d68685d487a790c9e1
Lnola <nola.luka@gmail.com>
2022-07-26 12:33:22 +0200

    Update api urls from functions to getters

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -3,16 +3,20 @@ import request from './config';
 
 const urls = {
   root: '/users',
-  signIn: () => urls.root + '/sign-in',
-  register: () => urls.root + '/register',
+  get signIn() {
+    return urls.root + '/sign-in';
+  },
+  get register() {
+    return urls.root + '/register';
+  },
 };
 
 const signIn = credentials => {
-  return request.post(urls.signIn(), credentials).then(extractData);
+  return request.post(urls.signIn, credentials).then(extractData);
 };
 
 const register = user => {
-  return request.post(urls.register(), user).then(extractData);
+  return request.post(urls.register, user).then(extractData);
 };
 
 export default {
