e5e88b29c2829241c6f3de94121346a8a3cacd82
sandje00 <stella.andjelic.00@fesb.hr>
2022-07-26 09:56:04 +0200

    Enable error to be instance of native JS Error in errorHandler

--- backend/src/shared/error/errorHandler.ts
+++ backend/src/shared/error/errorHandler.ts
@@ -3,8 +3,8 @@ import { IAuthRequest } from 'shared/auth/types';
 import { IHttpError } from './types';
 import { INTERNAL_SERVER_ERROR } from 'http-status';
 
-const errorHandler = (err: IHttpError, req: IAuthRequest, res: Response, _next: NextFunction) => {
-  return err.status
+const errorHandler = (err: IHttpError | Error, _req: IAuthRequest, res: Response, _next: NextFunction) => {
+  return 'status' in err
     ? res.status(err.status).json({ error: err.message })
     : res.status(INTERNAL_SERVER_ERROR).json({ error: 'Something went wrong' });
 };
