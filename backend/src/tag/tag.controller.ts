4c172adf0d3c23406e35fee107ae3b248fcfdf81
Lnola <nola.luka@gmail.com>
2022-07-27 14:23:26 +0200

    Handle error on tags get

--- backend/src/tag/tag.controller.ts
+++ backend/src/tag/tag.controller.ts
@@ -1,19 +1,23 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { Tag, User } from 'shared/database';
 
-const getAll = async ({ body: { userId } }: Request, res: Response) => {
-  const tags = await Tag.findAll({
-    include: {
-      model: User,
-      where: { id: userId },
-      required: false,
-      attributes: { exclude: ['firstName', 'lastName', 'username', 'password', 'refreshToken'] },
-      through: { attributes: [] },
-    },
-  });
+const getAll = async ({ body: { userId } }: Request, res: Response, next: NextFunction) => {
+  try {
+    const tags = await Tag.findAll({
+      include: {
+        model: User,
+        where: { id: userId },
+        required: false,
+        attributes: { exclude: ['firstName', 'lastName', 'username', 'password', 'refreshToken'] },
+        through: { attributes: [] },
+      },
+    });
 
-  const tagsToDisplay = tags.map(({ id, label, users }) => ({ id, label, selected: users.length !== 0 }));
-  return res.json(tagsToDisplay);
+    const tagsToDisplay = tags.map(({ id, label, users }) => ({ id, label, selected: users.length !== 0 }));
+    return res.json(tagsToDisplay);
+  } catch (err) {
+    next(err);
+  }
 };
 
 export { getAll };
