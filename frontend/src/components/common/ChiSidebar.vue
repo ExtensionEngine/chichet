edd7ab3f69094b1b14bcc4e506a51b8d212c40bb
Lnola <nola.luka@gmail.com>
2022-07-28 16:03:28 +0200

    Default sidebar styling

--- frontend/src/components/common/ChiSidebar.vue
+++ frontend/src/components/common/ChiSidebar.vue
@@ -1,29 +1,32 @@
 <template>
   <aside class="sidebar-aside">
-    <section>
-      <h2>Recomended Rooms</h2>
+    <section class="sidebar-section">
+      <h2 class="sidebar-header">Recomended Rooms</h2>
       <section class="sidebar-list" :style="{ maxHeight: '30vh' }">
-        <article class="sidebar-article">Text</article>
-        <article class="sidebar-article">Text</article>
-        <article class="sidebar-article">Text</article>
-        <article class="sidebar-article">Text</article>
+        <chi-card class="sidebar-card"></chi-card>
+      </section>
+    </section>
+
+    <section class="sidebar-section">
+      <h2 class="sidebar-header">Recomended Rooms</h2>
+      <section class="sidebar-list" :style="{ maxHeight: '30vh' }">
+        <chi-card class="sidebar-card"></chi-card>
       </section>
     </section>
   </aside>
 </template>
 
 <script>
-import getIconUrl from '@/utils/getIconUrl';
+import ChiCard from './ChiCard.vue';
 
 export default {
   name: 'room-list',
   props: {
     sections: { type: Object, required: true },
   },
-  setup() {
-    return { getIconUrl };
-  },
+
   emits: ['switch-sections', 'select'],
+  components: { ChiCard },
 };
 </script>
 
@@ -31,18 +34,36 @@ export default {
 .sidebar-aside {
   width: 33%;
   height: 100%;
-  background-color: var(--color-primary);
+  padding: 24px;
   border-bottom-right-radius: 120px;
+  background-color: var(--color-primary);
+  font: var(--font-accent);
+  color: var(--color-secondary);
+}
+
+.sidebar-section {
+  margin-bottom: 24px;
+}
+
+.sidebar-header {
+  margin: 0;
+  padding-bottom: 24px;
+  border-bottom: 1px solid var(--color-secondary);
 }
 
 .sidebar-list {
   width: 100%;
-  max-height: initial;
   display: inline-block;
+  max-height: initial;
   overflow-y: scroll;
 }
 
-.sidebar-article {
-  width: 100%;
+.sidebar-card {
+  margin-right: 8px;
+  padding: 12px;
+}
+
+.sidebar-card:first-of-type {
+  padding-top: 24px;
 }
 </style>
