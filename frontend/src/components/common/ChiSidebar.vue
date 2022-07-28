919f672bbdf07613f3bffbf12c9de69e83678165
Lnola <nola.luka@gmail.com>
2022-07-28 08:42:32 +0200

    Add sidebar with section titles

---
+++ frontend/src/components/common/ChiSidebar.vue
@@ -0,0 +1,41 @@
+<template>
+  <aside class="sidebar-wrapper">
+    <section v-for="{ label } in sections" :key="label" class="section-wrapper">
+      <h3 class="section-title">{{ label }}</h3>
+    </section>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'room-list',
+  props: {
+    sections: { type: Object, required: true },
+  },
+  setup(props) {
+    console.log(props);
+  },
+};
+</script>
+
+<style scoped>
+.sidebar-wrapper {
+  background-color: var(--color-primary);
+  width: 33%;
+  height: 100%;
+  padding: 24px;
+}
+
+.section-wrapper {
+  margin-bottom: 36px;
+}
+
+.section-title {
+  font: var(--font-accent);
+  font-size: var(--font-size-large);
+  color: var(--color-secondary);
+  margin: 0;
+  padding: 0 24px 24px 0;
+  border-bottom: 1px solid var(--color-secondary);
+}
+</style>
