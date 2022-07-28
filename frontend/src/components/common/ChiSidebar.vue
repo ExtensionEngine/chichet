2c145e1d9f106bbd3494edce260182fe0044e516
Lnola <nola.luka@gmail.com>
2022-07-28 09:08:14 +0200

    Add section items

--- frontend/src/components/common/ChiSidebar.vue
+++ frontend/src/components/common/ChiSidebar.vue
@@ -1,7 +1,21 @@
 <template>
   <aside class="sidebar-wrapper">
-    <section v-for="{ label } in sections" :key="label" class="section-wrapper">
+    <section v-for="{ label, elements } in sections" :key="label" class="section-wrapper">
       <h3 class="section-title">{{ label }}</h3>
+
+      <section class="elements-wrapper">
+        <span v-for="{ name, online } in elements" :key="name" class="elements-item">
+          <span class="elements-title">
+            <span class="elements-circle"></span>
+            <p class="elements-name">{{ name }}</p>
+          </span>
+
+          <span class="elements-title">
+            <span class="elements-online-amount">{{ online }}</span>
+            <span class="elements-online-profile"></span>
+          </span>
+        </span>
+      </section>
     </section>
   </aside>
 </template>
@@ -37,5 +51,45 @@ export default {
   margin: 0;
   padding: 0 24px 24px 0;
   border-bottom: 1px solid var(--color-secondary);
+}
+
+.elements-wrapper {
+  margin: 0 12px;
+}
+
+.elements-item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 24px;
+  font: var(--font-accent);
+  color: var(--color-secondary);
+}
+
+.elements-title {
+  display: flex;
+  align-items: center;
+}
+
+.elements-circle {
+  display: inline-block;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  background-color: var(--color-secondary);
+  margin-right: 16px;
+}
+
+.elements-name {
+  margin: 0;
+}
+
+.elements-online-profile {
+  display: inline-block;
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  background-color: var(--color-secondary);
+  margin-left: 4px;
 }
 </style>
