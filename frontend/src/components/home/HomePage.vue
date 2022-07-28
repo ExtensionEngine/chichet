8789b0c9507e0ab9cb360ebae090627f7ddf181b
Lnola <nola.luka@gmail.com>
2022-07-28 11:38:52 +0200

    Add sidebar to home :home:

--- frontend/src/components/home/HomePage.vue
+++ frontend/src/components/home/HomePage.vue
@@ -1,10 +1,14 @@
 <template>
   <main class="home-main">
+    <chi-sidebar @switch-sections="switchSections" :sections="sections[sectionsIndex]"></chi-sidebar>
     <button @click="authStore.signOut(router)">Sign out</button>
   </main>
 </template>
 
 <script>
+import { roomsSections, usersSections } from './constants';
+import ChiSidebar from '../common/ChiSidebar.vue';
+import { ref } from 'vue';
 import { useAuthStore } from '@/store/authStore';
 import { useRouter } from 'vue-router';
 
@@ -14,8 +18,16 @@ export default {
     const authStore = useAuthStore();
     const router = useRouter();
 
-    return { authStore, router };
+    const sectionsIndex = ref(0);
+    const sections = [roomsSections, usersSections];
+
+    const switchSections = () => {
+      sectionsIndex.value = (sectionsIndex.value + 1) % sections.length;
+    };
+
+    return { authStore, router, sections, sectionsIndex, switchSections };
   },
+  components: { ChiSidebar },
 };
 </script>
 
