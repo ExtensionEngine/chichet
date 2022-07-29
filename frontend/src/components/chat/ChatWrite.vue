6c6fcf5b683b7e0f8ef1408a191804b7526bd8f8
Lnola <nola.luka@gmail.com>
2022-07-29 16:25:16 +0200

    Send button styling

--- frontend/src/components/chat/ChatWrite.vue
+++ frontend/src/components/chat/ChatWrite.vue
@@ -1,10 +1,12 @@
 <template>
-  <section>
-    <form>
+  <section class="write-container">
+    <form class="write-form">
       <chi-field>
-        <chi-textarea></chi-textarea>
+        <chi-textarea placeholder="Write your message..." autofocus></chi-textarea>
       </chi-field>
-      <button></button>
+      <span class="write-send">
+        <img src="@/assets/img/send.svg" alt="send" />
+      </span>
     </form>
   </section>
 </template>
@@ -18,4 +20,23 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.write-container {
+  margin: 20px;
+}
+
+.write-form {
+  position: relative;
+}
+
+.write-send {
+  position: absolute;
+  bottom: 50%;
+  right: 28px;
+  cursor: pointer;
+}
+
+.write-send:hover {
+  opacity: 0.6;
+}
+</style>
